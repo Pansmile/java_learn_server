be5bc0e79147d06091c53b2051f8b8e34c8f2f12
Pansmile <yaonovichti@gmail.com>
2017-03-04 00:55:57 +0300

    ModuleManager now "implements" IModuleManager interface instead of "extends". Also IModuleManager interface methods are implemented in a basic non-functional way in order to avoid the compiler's warnings.

--- src/main/java/pro/i_it/learn/server/module/ModuleManager.java
+++ src/main/java/pro/i_it/learn/server/module/ModuleManager.java
@@ -1,12 +1,27 @@
 package pro.i_it.learn.server.module;
 
+import pro.i_it.learn.server.module.interfaces.IChatManager;
+import pro.i_it.learn.server.module.interfaces.IDataManager;
 import pro.i_it.learn.server.module.interfaces.IModuleManager;
+import pro.i_it.learn.server.module.interfaces.ISocketIOManager;
 
 /**
  * Created by syn on 01.03.17.
  */
-public class ModuleManager extends IModuleManager {
+public class ModuleManager implements IModuleManager {
+    @Override
+    public ISocketIOManager getSocketIOManager() {
+        return null;
+    }
 
+    @Override
+    public IChatManager getChatManager() {
+        return null;
+    }
 
-
+    @Override
+    public IDataManager getDataManager() {
+        return null;
+    }
 }
+
