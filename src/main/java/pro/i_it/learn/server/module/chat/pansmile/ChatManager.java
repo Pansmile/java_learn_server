5fbe835cd8458773337305d31709bbdef8f8026f
Pansmile <yaonovichti@gmail.com>
2017-03-07 16:07:12 +0300

    Code cleanup and stupidity fixing.

--- src/main/java/pro/i_it/learn/server/module/chat/pansmile/ChatManager.java
+++ src/main/java/pro/i_it/learn/server/module/chat/pansmile/ChatManager.java
@@ -2,6 +2,8 @@ package pro.i_it.learn.server.module.chat.pansmile;
 
 import pro.i_it.learn.server.model.MessageModel;
 import pro.i_it.learn.server.module.ModuleManager;
+import pro.i_it.learn.server.module.data.pansmile.MessageDao;
+import pro.i_it.learn.server.module.data.pansmile.UserDao;
 import pro.i_it.learn.server.module.interfaces.IChatManager;
 import pro.i_it.learn.server.module.interfaces.ISender;
 
@@ -12,16 +14,23 @@ import java.util.ArrayList;
  */
 public class ChatManager implements IChatManager {
     private ArrayList<ISender> senders;
+    private MessageDao messageDao;
 
     public ChatManager() {
         senders = new ArrayList<>();
     }
 
+    private void initMessageDao() {
+        if (messageDao == null) {
+            messageDao = (MessageDao) ModuleManager.getModuleManager().getDataManager().getMessageDao();
+        }
+    }
 
 
     @Override
     public void inputMessage(MessageModel model) {
-        ModuleManager.getModuleManager().getDataManager().getMessageDao().saveData(model);
+        initMessageDao();
+        messageDao.saveData(model);
         senders.forEach(sender -> sender.sendAll(model));
 
     }
@@ -35,6 +44,7 @@ public class ChatManager implements IChatManager {
 
     @Override
     public ArrayList<MessageModel> getHistory() {
-        return ModuleManager.getModuleManager().getDataManager().getMessageDao().getAllData();
+        initMessageDao();
+        return messageDao.getAllData();
     }
 }
