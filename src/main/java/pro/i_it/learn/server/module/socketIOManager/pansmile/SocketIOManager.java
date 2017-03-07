870790920e158503dfc57b9caf1001f362a7d614
Pansmile <yaonovichti@gmail.com>
2017-03-07 16:15:28 +0300

    Code cleanup and stupidity fixing.

--- src/main/java/pro/i_it/learn/server/module/socketIOManager/pansmile/SocketIOManager.java
+++ src/main/java/pro/i_it/learn/server/module/socketIOManager/pansmile/SocketIOManager.java
@@ -1,5 +1,10 @@
 package pro.i_it.learn.server.module.socketIOManager.pansmile;
 
+import com.corundumstudio.socketio.Configuration;
+import com.corundumstudio.socketio.SocketIOServer;
+import com.mysql.fabric.Server;
+import pro.i_it.learn.server.model.MessageModel;
+import pro.i_it.learn.server.module.ModuleManager;
 import pro.i_it.learn.server.module.interfaces.ISender;
 import pro.i_it.learn.server.module.interfaces.ISocketIOManager;
 
@@ -8,8 +13,41 @@ import pro.i_it.learn.server.module.interfaces.ISocketIOManager;
  * Created by studio on 04.03.17.
  */
 public class SocketIOManager implements ISocketIOManager {
+    private SocketIOServer server;
 
-    public SocketIOManager() {}
+    public SocketIOManager() {
+        try {
+            initServer();
+        } catch (Exception e0) {
+            e0.fillInStackTrace();
+        }
+    }
+
+    private void initServer() throws Exception {
+        if (server == null) {
+            Configuration config = new Configuration();
+            config.setPort(9090);
+            config.setHostname("localhost");
+            server = new SocketIOServer(config);
+            System.out.println("SERVER CREATED");
+            server.addEventListener("MESSAGE", String.class,
+                    (client, message, ackRequest) -> {
+                        System.out.println("Client said: " + message);
+                        ModuleManager.getModuleManager().getChatManager().inputMessage(
+                                new MessageModel(client.getNamespace().getName(), message));
+                    });
+
+        }
+    }
+
+    public void startSocketIOServer() throws Exception {
+        server.start();
+    }
+
+    public void stopSocketIOServer() throws Exception {
+        server.stop();
+    }
+
 
     @Override
     public ISender getSocketIOSender() {
--- src/main/java/pro/i_it/learn/server/servlet/InitializeListener.java
+++ src/main/java/pro/i_it/learn/server/servlet/InitializeListener.java
@@ -4,6 +4,7 @@ import com.corundumstudio.socketio.Configuration;
 import com.corundumstudio.socketio.SocketIOServer;
 import pro.i_it.learn.server.model.MessageModel;
 import pro.i_it.learn.server.module.ModuleManager;
+import pro.i_it.learn.server.module.socketIOManager.pansmile.SocketIOManager;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -14,44 +15,33 @@ import javax.servlet.annotation.WebListener;
  */
 @WebListener
 public class InitializeListener implements ServletContextListener {
-    private SocketIOServer server;
+    private SocketIOManager socketIOManager = (SocketIOManager) ModuleManager.getModuleManager().getSocketIOManager();
 
 
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
         System.out.println("startup");
         try {
-            initServer();
-            server.start();
+            socketIOManager.startSocketIOServer();
             System.out.println("SERVER STARTED!");
         } catch (Exception e0) {
             e0.fillInStackTrace();
         }
 
+
+
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent servletContextEvent) {
-        server.stop();
-        System.out.println("SERVER STOPPED!");
+        try {
+            socketIOManager.stopSocketIOServer();
+            System.out.println("SERVER STOPPED!");
+        } catch (Exception e0) {
+            e0.fillInStackTrace();
+        }
         System.out.println("shutdown");
 
     }
 
-   private void initServer() throws Exception {
-        if (server == null) {
-            Configuration config = new Configuration();
-            config.setPort(9090);
-            config.setHostname("localhost");
-            server = new SocketIOServer(config);
-            System.out.println("SERVER CREATED");
-            server.addEventListener("MESSAGE", String.class,
-                    (client, message, ackRequest) -> {
-                        System.out.println("Client said: " + message);
-                        ModuleManager.getModuleManager().getChatManager().inputMessage(
-                                new MessageModel(client.getNamespace().getName(), message));
-                    });
-
-        }
-    }
 }
